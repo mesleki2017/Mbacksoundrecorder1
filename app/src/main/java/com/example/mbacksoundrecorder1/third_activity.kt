313af9679b17015b0d07bc68d5723700443ce432
mesleki2017 <mesleki2017@gmail.com>
2023-01-18 14:48:28 +0000

    intent result la ilgili yeni tarz

--- app/src/main/java/com/example/mbacksoundrecorder1/third_activity.kt
+++ app/src/main/java/com/example/mbacksoundrecorder1/third_activity.kt
@@ -4,28 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import kotlinx.android.synthetic.main.activity_third.*
 
 class third_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
-
+        val previewRequest = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val mydata = it.data
+                val myuri= mydata?.data
+                imgv_1.setImageURI(myuri)
+                // do whatever with the data in the callback
+            }
+        }
     btn_3.setOnClickListener {
         Log.d("aaa","buton 3 e basıldı")
-        Intent(Intent.ACTION_GET_CONTENT).also {
-            it.type="image/*"
-            startActivityForResult(it,0)
-        }
+
+        val myintent1=Intent(Intent.ACTION_GET_CONTENT)
+        myintent1.type="image/*"
+        //startActivity(myintent1)
+        previewRequest.launch(myintent1)
     }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        val myuri=data?.data
-        Log.d("aaa",myuri.toString())
-        imgv_1.setImageURI(myuri)
-    }
 }
