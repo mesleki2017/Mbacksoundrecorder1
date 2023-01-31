e4c128ede98874705ce5285f24d34fcef0245276
mesleki2017 <mesleki2017@gmail.com>
2023-01-31 06:49:16 +0000

    grafik eklendi canli degil

--- app/src/main/java/com/example/mbacksoundrecorder1/MainActivity.kt
+++ app/src/main/java/com/example/mbacksoundrecorder1/MainActivity.kt
@@ -10,13 +10,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.core.app.ActivityCompat
 import com.example.mbacksoundrecorder1.databinding.ActivityMainBinding
-
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 
 class MainActivity : ComponentActivity() {
 
     lateinit var receiver: AirplaneModeChangeReceiver
     private lateinit var binding: ActivityMainBinding//The 'kotlin-android-extensions' Gradle plugin is deprecated
+    private  var ddd:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,8 @@ class MainActivity : ComponentActivity() {
         binding.btnStart.setOnClickListener {
             Log.d("aaa","start butonuna basıldı")
             binding.txtv1.text="Start"
+
+            setLineChartData(ddd.toFloat()*10)
 
             Intent(applicationContext, MyService::class.java).apply {
                 action = MyService.ACTION_START
@@ -70,6 +76,30 @@ class MainActivity : ComponentActivity() {
         }
 
     }
+
+    val linevalues = ArrayList<Entry>()
+    fun setLineChartData(deger1:Float) {
+
+        linevalues.add(Entry(deger1, deger1))
+
+        val linedataset = LineDataSet(linevalues, "First")
+        //We add features to our chart
+        linedataset.color = resources.getColor(R.color.purple_200)
+
+        linedataset.circleRadius = 10f
+        linedataset.setDrawFilled(true)
+        linedataset.valueTextSize = 20F
+        linedataset.fillColor = resources.getColor(androidx.appcompat.R.color.material_blue_grey_800)
+        linedataset.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        //We connect our data to the UI Screen
+        val data = LineData(linedataset)
+        binding.getTheGraph.data = data
+        binding.getTheGraph.setBackgroundColor(resources.getColor(R.color.white))
+        binding.getTheGraph.animateXY(2000, 2000, Easing.EaseInCubic)
+
+    }
+
     override fun onStop() {
         super.onStop()
         unregisterReceiver(receiver)
